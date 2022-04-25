8497a64ab4c7fa201321a6904b618fe6b45e82d6
Sakka Thamsawangsuk <Sakka.T@philliplife.com>
2022-04-25 21:48:45 +0700

    init source code

---
+++ DotNetOAuth.Server/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DotNetOAuth.Server
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //var builder = services.AddIdentityServer()
+            //    .AddInMemoryClients(Config.GetClients())
+            //    .AddInMemoryIdentityResources(Config.GetIdentityResources())
+            //    .AddInMemoryApiResources(Config.GetApis());
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients())
+                .AddResourceOwnerValidator<UserValidator>();
+
+            if (Environment.IsDevelopment())
+                builder.AddDeveloperSigningCredential();
+
+            else
+                throw new Exception("Configuration not defined");
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
